55a5d823109ce3964643ee1b2bc649fe29066146
Rob Gentry <97411164+robgentry999@users.noreply.github.com>
2022-03-16 15:43:40 -0500

    posting website

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//Code Snippet from
+//https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
+
+baguetteBox.run(".gallery");
+
+const search = new Filter('search', 'data-caption');
